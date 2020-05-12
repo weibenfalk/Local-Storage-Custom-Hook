071872e02b531204746fdf223d01c037db2c321a
Thomas Weibenfalk <thomas.weibenfalk@gmail.com>
2020-05-12 11:38:50 +0200

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useLocalStorage } from './useLocalStorage';
 
-function App() {
+const App: React.FC = () => {
+  const [state, setState] = useLocalStorage('testls');
+  console.log('re-render');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <p>Value: {state}</p>
+      <button onClick={() => setState(Math.floor(Math.random() * 10000))}>
+        Change state!
+      </button>
     </div>
   );
-}
+};
 
 export default App;
